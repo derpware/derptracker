4743bbfdd2d8aa889b80f2fa364cac32b8f62785
Felix Arndt <mail@felixarndt.de>
2013-02-22 14:06:08 +0100

    push all foursquare stats in one request

--- foursquare.php
+++ foursquare.php
@@ -6,10 +6,20 @@ $pachube = new PachubeAPI($cosm["apikey"]);
 
 $foursquareapi = new EpiFoursquare($foursquare["clientID"], $foursquare["clientSecret"], $foursquare["accesstoken"]);
 $checkin = $foursquareapi->get('/users/self');
-$checkins 	= json_decode($checkin->responseText)->response->user->checkins->count;
-$lat		= json_decode($checkin->responseText)->response->user->checkins->items[0]->venue->location->lat;
-$lng		= json_decode($checkin->responseText)->response->user->checkins->items[0]->venue->location->lng;
 
-$pachube->updateDatastream("csv", $foursquare["feedid"], "checkins", $checkins);
-$pachube->updateDatastream("csv", $foursquare["feedid"], "latitude", $lat);
-$pachube->updateDatastream("csv", $foursquare["feedid"], "longitude", $lng);
+$stream = array(
+	"checkins" 		=> json_decode($checkin->responseText)->response->user->checkins->count,
+	"mayorships" 	=> json_decode($checkin->responseText)->response->user->mayorships->count,
+	"friends"		=> json_decode($checkin->responseText)->response->user->friends->count,
+	"todos"			=> json_decode($checkin->responseText)->response->user->todos->count,
+	"latitude"		=> json_decode($checkin->responseText)->response->user->checkins->items[0]->venue->location->lat,
+	"longitude"		=> json_decode($checkin->responseText)->response->user->checkins->items[0]->venue->location->lng
+	);
+foreach ($stream as $item => $value) {
+	$streamitems[] = array("id" => "$item", "current_value" => "$value");
+}
+
+$data = array("datastreams" => $streamitems);
+$data = json_encode($data);
+var_dump($data);
+$pachube->updateFeed("json", $foursquare["feedid"], $data);
