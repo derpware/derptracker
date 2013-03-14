fabdbcff94770c5f01ce426ab62e91614d034aa4
Severin Schols <severin@schols.de>
2013-03-14 01:56:46 +0100

    More comments

--- cron.php
+++ cron.php
@@ -1,13 +1,16 @@
 <?php
 define("BASE_PATH", dirname(__FILE__));
 
+// Configuration storage
 require_once BASE_PATH.'/ConfigProvider.interface.php';
 require_once BASE_PATH.'/PHPFileConfig.class.php';
 ConfigProvider::create("PHPFileConfig");
 
+// Base Plugin classes
 require_once BASE_PATH.'/DataProvider.class.php';
 require_once BASE_PATH.'/StorageProvider.class.php';
 
+// Data Providers
 include(BASE_PATH.'/provider/foursquare.php');
 include(BASE_PATH.'/provider/trakt.php');
 include(BASE_PATH.'/provider/putio.php');
@@ -16,22 +19,26 @@ include(BASE_PATH.'/provider/appnet.php');
 include(BASE_PATH.'/provider/mail.php');
 include(BASE_PATH.'/provider/github.php');
 
+// Storage Providers
 include(BASE_PATH.'/storage/CosmStorage.class.php');
 
-
+// Find all storage providers
 $datastore_classes = array_filter(get_declared_classes(), function($className) {
 	return in_array('StorageProvider', class_parents($className));
 });
 
+// Instanciate all storage providers
 $datastores = array();
 foreach ($datastore_classes as $datastore_class) {
 	$datastores[] = new $datastore_class();
 }
 
+// find all data providers
 $providers = array_filter(get_declared_classes(), function($className) {
 	return in_array('DataProvider', class_parents($className));
 });
 
+// get data from each data provider, and push it to the storage providers
 foreach ($providers as $provider_class) {
 	$provider = new $provider_class();
 	if ($provider->isActive()) {
