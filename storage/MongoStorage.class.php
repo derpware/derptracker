ab3dd46885db14a2cf7b35ae199c3603d431d878
Felix Arndt <mail@felixarndt.de>
2013-03-29 16:26:25 +0100

    mongo: don't insert empty documents

--- storage/MongoStorage.class.php
+++ storage/MongoStorage.class.php
@@ -10,28 +10,33 @@ class MongoDBStorage extends StorageProvider {
 	public function putData($provider, $metadata, $rawdata) {
 		$provider = $provider->getName();
 		$timestamp = new MongoDate();
+
 		// Metadata
-		$metacollection = $this->db->$provider->meta;
-		$lastmeta = $metacollection->find()->sort(array('_timestamp' => -1))->limit(1);
-		foreach ($lastmeta as $val) {
-			unset($val['_timestamp'], $val['_id']);
-			$lastmeta = $val;
-		}
-		if($metadata != $lastmeta){
-			$metadata["_timestamp"] = $timestamp;
-			$metacollection->insert($metadata);
+		if($metadata){
+			$metacollection = $this->db->$provider->meta;
+			$lastmeta = $metacollection->find()->sort(array('_timestamp' => -1))->limit(1);
+			foreach ($lastmeta as $val) {
+				unset($val['_timestamp'], $val['_id']);
+				$lastmeta = $val;
+			}
+			if($metadata != $lastmeta){
+				$metadata["_timestamp"] = $timestamp;
+				$metacollection->insert($metadata);
+			}
 		}
 
 		// Rawdata
-		$rawcollection = $this->db->$provider->raw;
-		$lastraw = $rawcollection->find()->sort(array('_timestamp' => -1))->limit(1);
-		foreach ($lastraw as $val) {
-			unset($val['_timestamp'], $val['_id']);
-			$lastraw = $val;
-		}
-		if($rawdata != $lastraw){
-			$rawdata["_timestamp"] = $timestamp;
-			$rawcollection->insert($rawdata);
+		if($rawdata){
+			$rawcollection = $this->db->$provider->raw;
+			$lastraw = $rawcollection->find()->sort(array('_timestamp' => -1))->limit(1);
+			foreach ($lastraw as $val) {
+				unset($val['_timestamp'], $val['_id']);
+				$lastraw = $val;
+			}
+			if($rawdata != $lastraw){
+				$rawdata["_timestamp"] = $timestamp;
+				$rawcollection->insert($rawdata);
+			}
 		}
 	}
 
